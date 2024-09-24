d29df998a135087e9e005cf2c1d35bf36a6388b2
abh2-lab <abhinandan@boomlive.in>
2024-09-24 11:15:31 +0000

    first attempt

---
+++ introduction.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+
+
+def myfun():
+    st.title("Introduction page")
+    st.write("Welcome to the intro page!")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# Sidebar navigation
+st.sidebar.title("Explore the dashboard")
+selection = st.sidebar.radio(
+    "Choose a section",
+    ["Introduction", "Election data", "boomlive news data"]
 )
+
+#Display content based on selection
+if selection == "Introduction":
+    import introduction as intro
+    intro.myfun()
+elif selection == "Election data":
+    import election as elect
+    # awards.show()
+elif selection == "boomlive news data":
+    import boomdata as boomd
+    boomd.news_api_page()
+    
